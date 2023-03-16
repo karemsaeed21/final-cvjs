53c3895cf0326af9cd9c3478e9f4fad99bee2cbb
karemsaeed21 <109914324+karemsaeed21@users.noreply.github.com>
2023-03-16 21:45:34 +0200

    Add files via upload

---
+++ karem.js
@@ -0,0 +1,12 @@
+function viewLogInTab() {
+    document.getElementById('id01').style.display = 'block'
+}
+function closeLogInTab() {
+    document.getElementById('id01').style.display = 'none'
+}
+function openNav() {
+    document.getElementById('mySidenav').style.width = '250px'
+}
+function closeNav() {
+    document.getElementById('mySidenav').style.width = '0'
+}
